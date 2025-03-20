82a979ac82641f275d44ea7147c81c1d721024d8
Barnaby Malet <bmalet@gmail.com>
2025-03-20 23:32:06 +0100

    basic trpc server at backend

--- apps/backend/bin/backend.ts
+++ apps/backend/bin/backend.ts
@@ -1,11 +1,31 @@
 import { CONFIG_FILE_PATH, getLogger, readConfig } from "@director/core";
 import {} from "@director/core";
+import * as trpcExpress from "@trpc/server/adapters/express";
 import { Command } from "commander";
+import express from "express";
+import { appRouter } from "./router";
 
 const config = await readConfig(CONFIG_FILE_PATH);
 const program = new Command();
 
 const logger = getLogger("cli");
 
+// curl --location --globoff 'http://localhost:3000/trpc/greeting?input={%22name%22%3A%22somkiat%22}' \
+// --header 'Cookie: Cookie_1=value' | jq
+
 // Print out the full command that was called with all arguments
 logger.info(`Hello backend`);
+
+const app = express();
+const port = 3000;
+
+app.use(
+  "/trpc",
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+  }),
+);
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});

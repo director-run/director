397f312d8f6a33acb98d64a4247706e81642ea1a
Tom Bates <hey@tombates.co>
2025-03-23 15:49:55 -0300

    Adds themes

--- apps/website/next.config.ts
+++ apps/website/next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig = {
-  devIndicators: {
-    appIsrStatus: false,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
--- apps/website/src/app/page.tsx
+++ apps/website/src/app/page.tsx
@@ -4,6 +4,9 @@ export default async function HomePage() {
   return (
     <div className="flex h-[100dvh] w-full flex-col items-center justify-center p-8">
       <Logo />
+
+      <h1 className="font-bold font-sans text-4xl">This is geist</h1>
+      <h1 className="font-bold font-mono text-4xl">This is jetbrains mono</h1>
     </div>
   );
 }

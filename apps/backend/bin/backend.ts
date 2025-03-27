c68c1ae3872c490db4edb79ab7323ad990c100ed
Barnaby Malet <bmalet@gmail.com>
2025-03-27 23:22:54 +0100

    minor

--- apps/backend/bin/backend.ts
+++ apps/backend/bin/backend.ts
@@ -1,6 +1,9 @@
 console.log("Environment variables:");
-console.log("----------------");
 console.log(process.env);
+console.log("----------------");
+console.log("----------------");
+console.log("----------------");
+console.log("----------------");
 
 import { startServer } from "../src/http/server";
 import { listProxies } from "../src/services/listProxies";
@@ -8,40 +11,39 @@ import { listProxies } from "../src/services/listProxies";
 import path from "node:path";
 import { PrismaClient } from "@prisma/client";
 import { CONFIG_FILE_PATH } from "../src/config/env";
-import { readConfig } from "../src/config/readConfig";
 
 // Initialize the PrismaClient
 const prisma = new PrismaClient();
 
-const config = await readConfig(CONFIG_FILE_PATH);
-
 console.log(`Config file path: ${CONFIG_FILE_PATH}`);
-console.log("Config:");
-console.log(JSON.stringify(config));
-// Log the DATABASE_URL environment variable instead
+console.log("database URL", process.env.DATABASE_URL);
 console.log(
   `Prisma sqlite file: ${path.resolve(process.env.DATABASE_URL ?? "?")}`,
 );
+console.log("Directory name", __dirname);
+console.log("File name", __filename);
+console.log("----------------");
+console.log("----------------");
+console.log("----------------");
+console.log("----------------");
 
-const proxyData = {
-  name: "test-command-prxy",
-  servers: [
-    {
-      name: "Command Server",
-      transport: {
-        command: "uvx",
-        args: ["--from", "git+https://github.com/example/repo", "example"],
-      },
-    },
-  ],
-};
+// const proxyData = {
+//   name: "test-command-prxy",
+//   servers: [
+//     {
+//       name: "Command Server",
+//       transport: {
+//         command: "uvx",
+//         args: ["--from", "git+https://github.com/example/repo", "example"],
+//       },
+//     },
+//   ],
+// };
 
 // Act
 // await addProxy(proxyData);
 
 const proxies = await listProxies();
-console.log("Proxies:");
-console.log("----------------");
-console.log(proxies);
+console.log("Proxies:", proxies.length);
 
 startServer();

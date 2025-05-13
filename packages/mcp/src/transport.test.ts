a013485d0b938f76efc16027f8fb17da6e627237
Barnaby Malet <bmalet@gmail.com>
2025-05-13 19:14:25 +0200

    serving over stdio

--- packages/mcp/src/transport.test.ts
+++ packages/mcp/src/transport.test.ts
@@ -1,13 +1,25 @@
+import path from "path";
 import { describe, expect, test } from "vitest";
 import { SimpleClient } from "./simple-client";
 import { makeEchoServer } from "./test/fixtures";
 import { serveOverSSE } from "./transport";
-
 describe("transport", () => {
-  //   describe("serveOverStdio", () => {
-  //     test("should expose a server over stdio", async () => {
-  //     });
-  //   });
+  describe("serveOverStdio", () => {
+    test("should expose a server over stdio", async () => {
+      const basePath = __dirname;
+      const client = await SimpleClient.createAndConnectToStdio("bun", [
+        "-e",
+        `
+            import { makeEchoServer } from '${path.join(basePath, "test/fixtures.ts")}'; 
+            import { serveOverStdio } from '${path.join(basePath, "transport.ts")}'; 
+            serveOverStdio(makeEchoServer());
+        `,
+      ]);
+      const tools = await client.listTools();
+      expect(tools.tools).toHaveLength(1);
+      expect(tools.tools[0].name).toBe("echo");
+    });
+  });
 
   describe("serveOverSSE", () => {
     test("should expose a server over stdio", async () => {

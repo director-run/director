e9664f2238bbc577114a0b8237fb89ab64dce9c8
Barnaby Malet <bmalet@gmail.com>
2025-05-14 10:33:54 +0200

    minor

--- apps/cli/src/config.ts
+++ apps/cli/src/config.ts
@@ -8,7 +8,6 @@ export const env = createEnv({
   envVars: {
     GATEWAY_PORT: z.number({ coerce: true }).optional().default(3673),
     GATEWAY_URL: z.string().optional().default(`http://localhost:3673`),
-    DATA_DIR: z.string().optional().default(getDataDir()),
     DB_FILE_PATH: z
       .string()
       .optional()

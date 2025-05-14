4a6789b8f75424cebeb07ab640b1dea42874013f
Barnaby Malet <bmalet@gmail.com>
2025-05-14 18:13:31 +0200

    ok

--- packages/gateway/src/server.ts
+++ packages/gateway/src/server.ts
@@ -1,13 +1,11 @@
 import { Server } from "http";
 import { getLogger } from "@director.run/utilities/logger";
-import {
-  asyncHandler,
-  errorRequestHandler,
-} from "@director.run/utilities/middleware";
+import { errorRequestHandler } from "@director.run/utilities/middleware";
 import cors from "cors";
 import express from "express";
 import { Database } from "./db";
 import { ProxyServerStore } from "./proxy-server-store";
+import { createMCPRouter } from "./routers/mcp";
 import { createTRPCExpressMiddleware } from "./routers/trpc";
 
 const logger = getLogger("startService");
@@ -16,86 +14,65 @@ export const startService = async (attribs: {
   port: number;
   databaseFilePath: string;
 }) => {
-  const gateway = await Gateway.create(attribs);
-  await gateway.start();
+  const gateway = await Gateway.start(attribs);
   return gateway;
 };
 
 export class Gateway {
   public readonly proxyStore: ProxyServerStore;
   public readonly port: number;
-  private expressServer?: Server;
+  private server: Server;
 
   private constructor(attribs: {
     proxyStore: ProxyServerStore;
     port: number;
     db: Database;
+    server: Server;
   }) {
     this.port = attribs.port;
     this.proxyStore = attribs.proxyStore;
+    this.server = attribs.server;
   }
 
-  public static async create(attribs: {
+  public static async start(attribs: {
     port: number;
     databaseFilePath: string;
   }) {
-    const db = await Database.connect(attribs.databaseFilePath);
-    const proxyStore = await ProxyServerStore.create(db);
-
-    return new Gateway({
-      port: attribs.port,
-      db,
-      proxyStore,
-    });
-  }
-
-  public async start() {
     logger.info(`starting director...`);
 
+    const db = await Database.connect(attribs.databaseFilePath);
+    const proxyStore = await ProxyServerStore.create(db);
     const app = express();
 
     app.use(cors());
-
-    app.get(
-      "/:proxy_id/sse",
-      asyncHandler(async (req, res) => {
-        const proxyId = req.params.proxy_id;
-        const proxy = this.proxyStore.get(proxyId);
-        await proxy.startSSEConnection(req, res);
-      }),
-    );
-
-    app.post(
-      "/:proxy_id/message",
-      asyncHandler(async (req, res) => {
-        const proxyId = req.params.proxy_id;
-        const proxy = this.proxyStore.get(proxyId);
-        await proxy.handleSSEMessage(req, res);
-      }),
-    );
-
-    app.use(
-      "/trpc",
-      createTRPCExpressMiddleware({ proxyStore: this.proxyStore }),
-    );
-
+    app.use("/", createMCPRouter({ proxyStore }));
+    app.use("/trpc", createTRPCExpressMiddleware({ proxyStore }));
     app.use(errorRequestHandler);
 
-    this.expressServer = app.listen(this.port, () => {
-      logger.info(`director gateway running on port ${this.port}`);
+    const server = app.listen(attribs.port, () => {
+      logger.info(`director gateway running on port ${attribs.port}`);
+    });
+
+    const gateway = new Gateway({
+      port: attribs.port,
+      db,
+      proxyStore,
+      server,
     });
 
     process.on("SIGINT", async () => {
       logger.info("received SIGINT, cleaning up proxy servers...");
-      await this.stop();
+      await gateway.stop();
       process.exit(0);
     });
+
+    return gateway;
   }
 
   async stop() {
     await this.proxyStore.closeAll();
     await new Promise<void>((resolve) => {
-      this.expressServer?.close(() => resolve());
+      this.server.close(() => resolve());
     });
   }
 }

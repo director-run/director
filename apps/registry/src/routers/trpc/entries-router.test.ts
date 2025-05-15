75456c5c4c42d22a3fb62e57e3dd92ae4bedf9da
Barnaby Malet <bmalet@gmail.com>
2025-05-15 10:57:31 +0200

    ok

--- apps/registry/src/routers/trpc/entries-router.test.ts
+++ apps/registry/src/routers/trpc/entries-router.test.ts
@@ -1,7 +1,27 @@
-import { describe, it } from "vitest";
+import { afterAll, beforeAll, beforeEach, describe, it } from "vitest";
+import { env } from "../../config";
+import { Registry } from "../../registry";
+import { createTestEntries } from "../../test/fixtures/entries";
 
 describe("Entries Router", () => {
+  let registry: Registry;
+
+  beforeAll(async () => {
+    registry = await Registry.start({ port: env.REGISTRY_PORT });
+  });
+
+  afterAll(async () => {
+    await registry.stop();
+  });
+
+  beforeEach(async () => {
+    await registry.store.purge();
+  });
+
   describe("getEntries", () => {
-    it("should paginate properly", async () => {});
+    it("should handle pagination correctly", async () => {
+      const totalEntries = 100;
+      await registry.store.entries.addEntries(createTestEntries(totalEntries));
+    });
   });
 });

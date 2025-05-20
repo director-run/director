cdee644add76d460cea006f85e962d5fc04259f7
Barnaby Malet <bmalet@gmail.com>
2025-05-20 16:25:37 +0200

    ok

--- apps/registry/api/index.ts
+++ apps/registry/api/index.ts
@@ -1,7 +1,10 @@
-const express = require("express");
+import express from "express";
+
 const app = express();
 
-app.get("/", (req, res) => res.send("Registry API on Vercel"));
+app.get("/", (req, res) => {
+  res.send("Registry API on Vercel");
+});
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 

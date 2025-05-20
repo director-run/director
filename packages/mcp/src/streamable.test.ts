0f3ca9678524126a2eea2d7fff770c671147d59e
Barnaby Malet <bmalet@gmail.com>
2025-05-20 19:28:29 +0200

    ok

--- packages/mcp/src/streamable.test.ts
+++ packages/mcp/src/streamable.test.ts
@@ -1,7 +1,16 @@
-import { describe, test } from "vitest";
+import type { Server } from "node:http";
+import { beforeAll, describe, test } from "vitest";
 import { SimpleClient } from "./simple-client";
+import { serveOverStreamable } from "./streamable";
+import { makeEchoServer } from "./test/fixtures";
 
 describe("SimpleServer", () => {
+  let server: Server;
+
+  beforeAll(async () => {
+    server = await serveOverStreamable(makeEchoServer(), 3000);
+  });
+
   test("should create a server with a tool", async () => {
     // const server = new SimpleServer();
     const client = await SimpleClient.createAndConnectToStreamable(

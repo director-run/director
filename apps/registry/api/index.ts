651b7edf56bba8910b58379232bb4439a01a982e
Barnaby Malet <bmalet@gmail.com>
2025-05-21 22:36:43 +0200

    try building file

--- apps/registry/api/index.ts
+++ apps/registry/api/index.ts
@@ -1,16 +1,1 @@
-// import express from "express";
-
-import { Registry } from "../src/registry";
-
-// const app = express();
-
-// app.get("/", (req, res) => {
-//   res.send("Function: api/index.ts");
-// });
-
-// app.listen(3000, () => console.log("Server ready on port 3000."));
-
-// module.exports = app;
-const PORT = 3000;
-const registry = Registry.start({ port: PORT });
-module.exports = registry.app;
+export default require("../dist/api.js");
---
+++ apps/registry/bin/api.ts
@@ -0,0 +1,16 @@
+// import express from "express";
+
+import { Registry } from "../src/registry";
+
+// const app = express();
+
+// app.get("/", (req, res) => {
+//   res.send("Function: api/index.ts");
+// });
+
+// app.listen(3000, () => console.log("Server ready on port 3000."));
+
+// module.exports = app;
+const PORT = 3000;
+const registry = Registry.start({ port: PORT });
+module.exports = registry.app;

5e94b04decc9beb87e79ea2c2555c8bfe91a5a5d
Barnaby Malet <bmalet@gmail.com>
2025-05-22 12:10:03 +0200

    registry .env.local

--- apps/registry/src/config.ts
+++ apps/registry/src/config.ts
@@ -1,5 +1,6 @@
 import path from "path";
 import { createEnv, isProduction, isTest } from "@director.run/utilities/env";
+import { isFilePresent } from "@director.run/utilities/os";
 import { z } from "zod";
 
 export const env = createEnv({
@@ -11,7 +12,9 @@ export const env = createEnv({
 });
 
 function getEnvFilePath() {
-  if (isTest()) {
+  if (isFilePresent(path.join(__dirname, "../.env.local"))) {
+    return path.join(__dirname, "../.env.local");
+  } else if (isTest()) {
     return path.join(__dirname, "../env/.env.test");
   } else if (isProduction()) {
     return path.join(__dirname, "../env/.env");

3470f972aae9dc4fca771decab1c9afa111d6483
Barnaby Malet <bmalet@gmail.com>
2025-05-23 20:02:07 +0200

    minor

--- apps/registry/src/registry.ts
+++ apps/registry/src/registry.ts
@@ -1,5 +1,4 @@
 import { Server } from "http";
-import { isProduction } from "@director.run/utilities/env";
 import { getLogger } from "@director.run/utilities/logger";
 import { errorRequestHandler } from "@director.run/utilities/middleware";
 import { notFoundHandler } from "@director.run/utilities/middleware";
@@ -41,12 +40,11 @@ export class Registry {
     app.use(express.json());
     app.use("/trpc", createTRPCExpressMiddleware({ store }));
 
-    if (isProduction()) {
-      app.get("/", (req, res) => res.redirect("https://director.run"));
-    }
+    // if (isProduction()) {
+    //   app.get("/", (req, res) => res.redirect("https://director.run"));
+    // }
 
-    app.get("*", notFoundHandler);
-    app.post("*", notFoundHandler);
+    app.all("*", notFoundHandler);
     app.use(errorRequestHandler);
 
     const server = app.listen(params.port, () => {

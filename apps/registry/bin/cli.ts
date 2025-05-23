0f970d6f4e57ecfdda7a67f501a3b7de12b6e902
Barnaby Malet <bmalet@gmail.com>
2025-05-23 20:06:25 +0200

    remove unneeded registry cli commands

--- apps/registry/bin/cli.ts
+++ apps/registry/bin/cli.ts
@@ -3,7 +3,6 @@
 import { Command } from "commander";
 import packageJson from "../package.json";
 import { registerEntriesCommands } from "../src/commands/entries";
-import { registerServerCommands } from "../src/commands/server";
 import { env } from "../src/config";
 import { createStore } from "../src/db/store";
 
@@ -17,7 +16,6 @@ program
 const store = createStore({ connectionString: env.DATABASE_URL });
 
 program.addCommand(registerEntriesCommands(store));
-program.addCommand(registerServerCommands());
 
 program.hook("postAction", async () => {
   await store.close();
--- apps/registry/src/commands/entries.ts
+++ apps/registry/src/commands/entries.ts
@@ -1,8 +1,7 @@
-import { actionWithErrorHandler, makeTable } from "@director.run/utilities/cli";
+import { actionWithErrorHandler } from "@director.run/utilities/cli";
 import { Command } from "commander";
 import { type Store } from "../db/store";
 import { enrichEntries } from "../enrichment/enrich";
-import { prettyPrint } from "../helpers/pretty-print";
 import { fetchRaycastRegistry } from "../importers/raycast";
 import { getSeedEntries } from "../importers/seed";
 export function registerEntriesCommands(store: Store) {
@@ -43,49 +42,6 @@ export function registerEntriesCommands(store: Store) {
     .action(
       actionWithErrorHandler(async () => {
         console.log(await store.entries.getStatistics());
-      }),
-    );
-
-  command
-    .command("ls")
-    .description("List all entries")
-    .action(
-      actionWithErrorHandler(async () => {
-        const entries = await store.entries.getAllEntries();
-        const table = makeTable(["Name", "Command", "hasReadme", "parameters"]);
-        for (const entry of entries) {
-          if (entry.transport.type === "stdio") {
-            table.push([
-              entry.name,
-              [entry.transport.command, ...entry.transport.args].join(" "),
-              !!entry.readme,
-              entry.parameters?.map((p) => p.name).join(", "),
-            ]);
-          }
-        }
-        console.log(table.toString());
-      }),
-    );
-
-  command
-    .command("readme <name>")
-    .description("show the readme of the entry behind the name")
-    .action(
-      actionWithErrorHandler(async (name: string) => {
-        const entry = await store.entries.getEntryByName(name);
-        console.log(entry.readme);
-      }),
-    );
-
-  command
-    .command("get <name>")
-    .description(
-      "Pretty print (with colours unless it's super verbose) the json object of the entry behind the name",
-    )
-    .action(
-      actionWithErrorHandler(async (name: string) => {
-        const entry = await store.entries.getEntryByName(name);
-        console.log(prettyPrint(entry, { indentSize: 2, padding: 1 }));
       }),
     );
 

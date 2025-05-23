c22963c056382ca4382c972db0014d797a945ca2
Barnaby Malet <bmalet@gmail.com>
2025-05-23 21:57:28 +0200

    minor

--- apps/registry/src/routers/trpc/entries-router.ts
+++ apps/registry/src/routers/trpc/entries-router.ts
@@ -1,7 +1,6 @@
-import { AppError, ErrorCode } from "@director.run/utilities/error";
 import { t } from "@director.run/utilities/trpc";
 import { z } from "zod";
-import { env } from "../../config";
+import { protectedProcedure } from ".";
 import type { Store } from "../../db/store";
 
 export function createEntriesRouter({ store }: { store: Store }) {
@@ -23,11 +22,3 @@ export function createEntriesRouter({ store }: { store: Store }) {
       .query(({ input }) => store.entries.getEntryByName(input.name)),
   });
 }
-
-export const protectedProcedure = t.procedure.use(function isAuthed(opts) {
-  const apiKey = ""; //opts.ctx.req.headers.get("x-api-key");
-  if (apiKey !== env.API_WRITE_KEY) {
-    throw new AppError(ErrorCode.UNAUTHORIZED, "Unauthorized");
-  }
-  return opts.next();
-});

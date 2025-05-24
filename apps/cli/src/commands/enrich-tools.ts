cd6e50f8dd2d56430018802284e512399b181ae3
Barnaby Malet <bmalet@gmail.com>
2025-05-24 22:30:09 +0200

    enrich tools doing it's thing

--- apps/cli/src/commands/enrich-tools.ts
+++ apps/cli/src/commands/enrich-tools.ts
@@ -1,5 +1,10 @@
 import { SimpleClient } from "@director.run/mcp/simple-client";
+import { type EntryGetParams } from "@director.run/registry/db/schema";
+import {} from "@director.run/utilities/error";
+import { getLogger } from "@director.run/utilities/logger";
 import { registryClient } from "../client";
+
+const logger = getLogger("enrich-tools");
 
 export async function enrichTools() {
   const entries = await registryClient.entries.getEntries.query({
@@ -8,23 +13,32 @@ export async function enrichTools() {
   });
 
   for (const entry of entries.entries) {
-    console.log(`enriching tool ${entry.name}`);
-    const client = new SimpleClient(`${entry.name}-client`);
-    const transport = entry.transport;
-    console.log(transport);
-    if (transport.type === "stdio") {
-      console.log(`connecting to stdio ${entry.name}...`);
-      await client.connectToStdio(transport.command, transport.args, {
-        ...process.env,
-        ...transport.env,
-      });
-      console.log(`connected to stdio ${entry.name}, fetching tools...`);
-      const tools = await client.listTools();
-      //   console.log(JSON.stringify(tools, null, 2));
-      console.log(`closing client ${entry.name}`);
-      await client.close();
-    } else {
-      console.log("not supported", transport.type);
+    try {
+      await fetchToolsForEntry(entry);
+    } catch (error) {
+      logger.error(`error enriching ${entry.name}: ${error}`);
     }
+  }
+}
+
+async function fetchToolsForEntry(entry: EntryGetParams) {
+  const transport = entry.transport;
+  const client = new SimpleClient(`${entry.name}-client`);
+  logger.info(`processing ${entry.name}...`);
+
+  if (transport.type === "stdio") {
+    await client.connectToStdio(transport.command, transport.args, {
+      ...process.env,
+      ...transport.env,
+    });
+    logger.info(`connected to ${entry.name}, fetching tools...`);
+    const tools = await client.listTools();
+    //   console.log(JSON.stringify(tools, null, 2));
+    logger.info(`closing client ${entry.name}`);
+    await client.close();
+  } else {
+    logger.warn(
+      `skipping ${entry.name}, unsupported transport type: ${transport.type}`,
+    );
   }
 }

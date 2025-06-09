796a27314f812b678dddde41a94a9ebc81f7a3ff
Barnaby Malet <bmalet@gmail.com>
2025-06-09 23:37:03 +0200

    remove legacy ssh commands

--- apps/sandbox/ssh.sh
+++
@@ -1,9 +0,0 @@
-if [ -z "$1" ]; then
-  echo "Usage: $0 <vm-name>" >&2
-  exit 1
-fi
-
-VM_NAME="$1"
-PASSWORD="admin"  # default password is "admin"
-
-sshpass -p "$PASSWORD" ssh -o StrictHostKeyChecking=no admin@$(tart ip "$VM_NAME")

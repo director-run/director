714ea9982228b68bf40ed7a4d0fae9be8f4acd7d
Barnaby Malet <bmalet@gmail.com>
2025-06-09 23:24:34 +0200

    minor

--- apps/sandbox/src/commands/start.ts
+++ apps/sandbox/src/commands/start.ts
@@ -1,6 +1,6 @@
 import { $ } from "zx";
 
-export async function start(name: string) {
+export async function start(name: string, options: { mount?: string } = {}) {
   console.log(`starting ${name}`);
   const run = await $`nohup tart run ${name} --no-graphics &`;
   return run;

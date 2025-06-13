921ac4fdcb3ba9146496b131f7f75fe19ead634d
Barnaby Malet <bmalet@gmail.com>
2025-06-13 15:26:00 +0200

    ok

--- apps/registry/bin/test-entry.ts
+++ apps/registry/bin/test-entry.ts
@@ -1,11 +1,11 @@
 import { createGatewayClient } from "@director.run/gateway/client";
 import { getStreamablePathForProxy } from "@director.run/gateway/helpers";
 import { SimpleClient } from "@director.run/mcp/simple-client";
-import { blue, whiteBold, yellow } from "@director.run/utilities/cli/colors";
+import { blue, yellow } from "@director.run/utilities/cli/colors";
 import { makeTable } from "@director.run/utilities/cli/index";
 import { getLogger } from "@director.run/utilities/logger";
 import { joinURL } from "@director.run/utilities/url";
-import { input } from "@inquirer/prompts";
+import { input, select } from "@inquirer/prompts";
 import type { Tool } from "@modelcontextprotocol/sdk/types.js";
 import type { EntryCreateParams, EntryGetParams } from "../src/db/schema";
 import { parseParameters } from "../src/enrichment/parse-parameters";
@@ -25,6 +25,10 @@ export async function getMCPClientForEntry({
 
   console.log("");
   console.log("");
+  console.log(yellow("******************"));
+  console.log(yellow("* ENRICHED ENTRY *"));
+  console.log(yellow("******************"));
+  console.log("");
   console.log(enrichedEntry);
   console.log("");
   console.log("");
@@ -33,7 +37,11 @@ export async function getMCPClientForEntry({
   const resolvedTransport = interpolateParameters(enrichedEntry, parameters);
 
   console.log("");
-  console.log("resolvedTransport:", resolvedTransport);
+  console.log(yellow("******************"));
+  console.log(yellow("*   TRANSPORT    *"));
+  console.log(yellow("******************"));
+  console.log("");
+  console.log(resolvedTransport);
   console.log("");
 
   logger.info("reseting gateway...");
@@ -56,12 +64,54 @@ export async function getMCPClientForEntry({
   const tools = await mcpClient.listTools();
   printTools(tools.tools);
 
+  if (tools.tools.length === 0) {
+    throw new Error("No tools found");
+  }
+  // Choose a tool to execute
+
+  const answer = await select({
+    message: "select a tool to run",
+    choices: tools.tools.map((tool) => {
+      return {
+        name: tool.name,
+        value: tool.name,
+        description: tool.description,
+      };
+    }),
+  });
+  console.log(yellow("******************"));
+  console.log(yellow(`* TOOL CALL: ${answer} *`));
+  console.log(yellow("******************"));
+  console.log("");
+
+  const toolToRun = tools.tools.find((tool) => tool.name === answer);
+  if (!toolToRun) {
+    throw new Error("Tool not found");
+  }
+  console.log(yellow(`* INPUT SCHEMA *`));
+  console.log(toolToRun.inputSchema);
+  console.log("");
+
+  // console.log("");
+  // console.log(yellow(`* TOOL CALL: ${answer} *`));
+  // console.log("");
+
+  // const parameters = await promptForParameters(toolToRun.inputSchema);
+  // console.log(parameters);
+
+  // const result = await mcpClient.callTool({
+  //   name: answer,
+  //   arguments: {},
+  // });
+
   return mcpClient;
 }
 
 function printTools(tools: Tool[]) {
   console.log("");
-  console.log(whiteBold("TOOLS"));
+  console.log(yellow("******************"));
+  console.log(yellow("*     TOOLS     *"));
+  console.log(yellow("******************"));
   console.log("");
 
   for (const tool of tools) {

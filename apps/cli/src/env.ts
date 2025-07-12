599781d631fac8f8ba1e3c4a1b8f532d80de70b7
Barnaby Malet <bmalet@gmail.com>
2025-07-12 19:18:49 +0200

    add production write key

--- apps/cli/src/env.ts
+++ apps/cli/src/env.ts
@@ -6,13 +6,18 @@ import { z } from "zod";
 
 export const LOCAL_ENV_FILE_PATH = path.join(process.cwd(), ".env.local");
 
+const SEGMENT_PRODUCTION_WRITE_KEY = "Z8wjEfWMFnlltCpGPPWlvsEQH1aVEUH3";
+
 export const env = createEnv({
   envFilePath: getEnvFilePath(),
   envVars: {
     GATEWAY_PORT: z.number({ coerce: true }).optional().default(3673),
     GATEWAY_URL: z.string().optional().default(`http://localhost:3673`),
     STUDIO_URL: z.string().optional().default(`https://studio.director.run`),
-    SEGMENT_WRITE_KEY: z.string().optional().default(""),
+    SEGMENT_WRITE_KEY: z
+      .string()
+      .optional()
+      .default(SEGMENT_PRODUCTION_WRITE_KEY),
     REGISTRY_API_URL: z
       .string()
       .optional()

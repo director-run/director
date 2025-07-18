1bee1642af487705c06f8e43e842956c0a475c40
Tom Bates <hey@tombates.co>
2025-07-18 10:47:58 -0300

    WIP

--- apps/registry/src/lib/enrich-entries.ts
+++ apps/registry/src/lib/enrich-entries.ts
@@ -1,20 +1,27 @@
 import { getLogger } from "@director.run/utilities/logger";
 import type { RegistryEntry } from "@director.run/utilities/schema";
 
+import { execSync } from "child_process";
 import { type EntryStore } from "../db/entries";
-import { getGithubRawReadmeUrl, isGithubRepo } from "./github";
+import { isGithubRepo } from "./github";
 
 const logger = getLogger("enrich");
 
 export async function enrichEntries(store: EntryStore) {
   const entries = await store.getAllEntries();
+
+  const enriched = await enrichEntry(entries[0]);
+  console.log({ enriched });
+
+  return;
   for (const entry of entries) {
     if (entry.isEnriched) {
       logger.info(`skipping ${entry.name}: already enriched`);
     } else {
       try {
         const enriched = await enrichEntry(entry);
-        await store.updateEntry(entry.id, enriched);
+        console.log({ enriched });
+        // await store.updateEntry(entry.id, enriched);
       } catch (error) {
         logger.error(`error enriching ${entry.name}: ${error}`);
       }
@@ -25,12 +32,14 @@ export async function enrichEntries(store: EntryStore) {
 async function enrichEntry(entry: RegistryEntry): Promise<RegistryEntry> {
   logger.info(`enriching ${entry.name}`);
   if (isGithubRepo(entry.homepage)) {
-    const response = await fetch(getGithubRawReadmeUrl(entry.homepage));
-    return {
-      ...entry,
-      readme: await response.text(),
-      isEnriched: true,
-    };
+    const thing = execSync(`gh`);
+    console.log(thing);
+    // const response = await fetch(getGithubRawReadmeUrl(entry.homepage));
+    // return {
+    //   ...entry,
+    //   readme: await response.text(),
+    //   isEnriched: true,
+    // };
   } else {
     return entry;
   }

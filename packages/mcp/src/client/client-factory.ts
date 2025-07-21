b2ec4f0bc849d41ba5a6b1eb04ebe4accf8333fd
Barnaby Malet <bmalet@gmail.com>
2025-07-21 21:17:45 +0200

    client factory

---
+++ packages/mcp/src/client/client-factory.ts
@@ -0,0 +1,20 @@
+import type { ProxyTargetAttributes } from "@director.run/utilities/schema";
+import { HTTPClient } from "./http-client";
+import { StdioClient } from "./stdio-client";
+
+export function createClientForTarget(target: ProxyTargetAttributes) {
+  switch (target.transport.type) {
+    case "http":
+      return new HTTPClient({
+        url: target.transport.url,
+        name: target.name,
+      });
+    case "stdio":
+      return new StdioClient({
+        name: target.name,
+        command: target.transport.command,
+        args: target.transport.args,
+        env: target.transport.env,
+      });
+  }
+}

a292c2596e6b8bfb6584ca7a9b34344647a2c075
Barnaby Malet <bmalet@gmail.com>
2025-07-23 14:06:19 +0200

    stdio client tests

--- packages/mcp/src/client/stdio-client.test.ts
+++ packages/mcp/src/client/stdio-client.test.ts
@@ -1,7 +1,36 @@
-import { describe, test } from "vitest";
+import { describe, expect, it } from "vitest";
+import { StdioClient } from "./stdio-client";
 
-describe("HTTPClient", () => {
-  describe("throwOnError", () => {
-    test("should throw on error", async () => {});
+describe("StdioClient", () => {
+  describe("connectToTarget", () => {
+    describe("error handling", () => {
+      it("should fail if it can't connect to stdio", async () => {
+        const client = new StdioClient({
+          name: "echo",
+          command: "not_existing_command",
+          args: [],
+        });
+
+        await expect(
+          client.connectToTarget({ throwOnError: true }),
+        ).rejects.toThrow(
+          `[echo] command not found: 'not_existing_command'. Please make sure it is installed and available in your $PATH.`,
+        );
+      });
+
+      it("should bubble up command errors properly", async () => {
+        const client = new StdioClient({
+          name: "echo",
+          command: "ls",
+          args: ["not_existing_dir"],
+        });
+
+        await expect(
+          client.connectToTarget({ throwOnError: true }),
+        ).rejects.toThrow(
+          `[echo] failed to run 'ls not_existing_dir'. Please check the logs for more details.`,
+        );
+      });
+    });
   });
 });

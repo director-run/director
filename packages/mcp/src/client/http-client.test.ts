854df7b1cd5ddd962a1744c80809a3597ec282a4
Barnaby Malet <bmalet@gmail.com>
2025-07-23 12:41:31 +0200

    tidy

--- packages/mcp/src/client/http-client.test.ts
+++ packages/mcp/src/client/http-client.test.ts
@@ -8,45 +8,42 @@ import { HTTPClient } from "./http-client";
 
 describe("HTTPClient", () => {
   describe("connectToTarget", () => {
-    describe("when connecting to a streamable server", () => {
-      test("should connect properly", async () => {
-        const instance = await serveOverStreamable(makeEchoServer(), 2345);
-        const client = new HTTPClient({
-          name: "test-client",
-          url: "http://localhost:2345/mcp",
-        });
-        await client.connectToTarget({ throwOnError: true });
-
-        expect(client.status).toBe("connected");
-        expect(client.lastErrorMessage).toBeUndefined();
-
-        const tools = await client.listTools();
-        expect(tools.tools).toHaveLength(1);
-        expect(tools.tools[0].name).toBe("echo");
-        await instance.close();
+    test("should connect to a streamable server", async () => {
+      const instance = await serveOverStreamable(makeEchoServer(), 2345);
+      const client = new HTTPClient({
+        name: "test-client",
+        url: "http://localhost:2345/mcp",
       });
-    });
-    describe("when connecting to a sse server", () => {
-      test("should connect properly", async () => {
-        const instance = await serveOverSSE(makeEchoServer(), 2345);
+      await client.connectToTarget({ throwOnError: true });
 
-        const client = new HTTPClient({
-          name: "test-client",
-          url: "http://localhost:2345/sse",
-        });
-        await client.connectToTarget({ throwOnError: true });
+      expect(client.status).toBe("connected");
+      expect(client.lastErrorMessage).toBeUndefined();
 
-        expect(client.status).toBe("connected");
-        expect(client.lastErrorMessage).toBeUndefined();
-
-        const tools = await client.listTools();
-        expect(tools.tools).toHaveLength(1);
-        expect(tools.tools[0].name).toBe("echo");
-        await instance.close();
-      });
+      const tools = await client.listTools();
+      expect(tools.tools).toHaveLength(1);
+      expect(tools.tools[0].name).toBe("echo");
+      await instance.close();
     });
 
-    describe("failures", () => {
+    test("should connect to an sse server", async () => {
+      const instance = await serveOverSSE(makeEchoServer(), 2345);
+
+      const client = new HTTPClient({
+        name: "test-client",
+        url: "http://localhost:2345/sse",
+      });
+      await client.connectToTarget({ throwOnError: true });
+
+      expect(client.status).toBe("connected");
+      expect(client.lastErrorMessage).toBeUndefined();
+
+      const tools = await client.listTools();
+      expect(tools.tools).toHaveLength(1);
+      expect(tools.tools[0].name).toBe("echo");
+      await instance.close();
+    });
+
+    describe("error handling", () => {
       // test("oauth unauthorized", async () => {
       //   const client = new HTTPClient({
       //     name: "test-client",

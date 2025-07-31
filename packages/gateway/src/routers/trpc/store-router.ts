8d928fe691e289c76dfc7562d063f9a2578df16f
Barnaby Malet <bmalet@gmail.com>
2025-07-31 18:00:07 +0200

    tool calling trpc endpoint

--- packages/gateway/src/routers/trpc/store-router.ts
+++ packages/gateway/src/routers/trpc/store-router.ts
@@ -94,6 +94,24 @@ export function createProxyStoreRouter({
         return await serializeProxyServerTarget(target, input.queryParams);
       }),
 
+    callTool: t.procedure
+      .input(
+        z.object({
+          proxyId: z.string(),
+          serverName: z.string(),
+          toolName: z.string(),
+          arguments: z.any(),
+        }),
+      )
+      .mutation(async ({ input }) => {
+        const proxy = await proxyStore.get(input.proxyId);
+        const target = await proxy.getTarget(input.serverName);
+        return await target.originalCallTool({
+          name: input.toolName,
+          arguments: input.arguments,
+        });
+      }),
+
     updateServer: t.procedure
       .input(
         z.object({

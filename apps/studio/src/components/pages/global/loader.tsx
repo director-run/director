0c07f85ecc234c7fe2bd16f53e6376e35865abc6
Barnaby Malet <bmalet@gmail.com>
2025-09-12 23:51:40 +0200

    remove some components

--- apps/studio/src/components/pages/global/loader.tsx
+++
@@ -1,9 +0,0 @@
-import { Logo } from "../../ui/icons/logo";
-
-export const FullScreenLoader = () => {
-  return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      <Logo className="size-10 animate-pulse" />
-    </div>
-  );
-};

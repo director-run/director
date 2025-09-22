192539d5396d3a6901a433e24018a8ebe52ea0a8
Barnaby Malet <bmalet@gmail.com>
2025-09-22 15:47:45 +0200

    ok

--- apps/studio-vite/src/hooks/use-add-server.ts
+++ apps/studio-vite/src/hooks/use-add-server.ts
@@ -8,17 +8,11 @@ export function useAddServer(options?: AddServerMutationOptions) {
   const utils = gatewayClient.useUtils();
 
   const mutation = gatewayClient.store.addServer.useMutation({
+    ...options,
     async onSuccess(data, variables, context) {
       await utils.store.getAll.invalidate();
       await utils.store.get.invalidate({ proxyId: variables.proxyId });
-      if (options && options.onSuccess) {
-        await options.onSuccess(data, variables, context);
-      }
-    },
-    onError(error, variables, context) {
-      if (options && options.onError) {
-        options.onError(error, variables, context);
-      }
+      await options?.onSuccess?.(data, variables, context);
     },
   });
 

f09c29dfcba35e50b68c69f6a7055c211aeccdbd
Barnaby Malet <bmalet@gmail.com>
2025-09-22 15:34:36 +0200

    minor

--- apps/studio-vite/src/hooks/use-registry-entry.ts
+++ apps/studio-vite/src/hooks/use-registry-entry.ts
@@ -3,10 +3,12 @@ import { registryClient } from "../contexts/gateway-context";
 export function useRegistryEntry(params: {
   entryName?: string;
 }) {
-  if (!params.entryName) {
-    throw new Error("Entry name is required");
-  }
-  return registryClient.entries.getEntryByName.useQuery({
-    name: params.entryName,
-  });
+  return registryClient.entries.getEntryByName.useQuery(
+    {
+      name: params.entryName ?? "",
+    },
+    {
+      enabled: !!params.entryName,
+    },
+  );
 }

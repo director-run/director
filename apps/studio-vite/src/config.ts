e843b46b98c16c1a2c72749d77dfe2e8d2ae4e15
Barnaby Malet <bmalet@gmail.com>
2025-09-24 14:36:19 +0200

    minor

--- apps/studio-vite/src/config.ts
+++ apps/studio-vite/src/config.ts
@@ -1,10 +1,11 @@
 const appConfig = (
-  window as unknown as { __APP_CONFIG__: Record<string, unknown> | undefined }
+  window as unknown as { __APP_CONFIG__: Record<string, string> | undefined }
 ).__APP_CONFIG__;
 
-export const GATEWAY_URL = appConfig?.gatewayUrl || "http://localhost:3673";
-export const REGISTRY_URL =
+export const GATEWAY_URL: string =
+  appConfig?.gatewayUrl || "http://localhost:3673";
+export const REGISTRY_URL: string =
   appConfig?.registryUrl || "https://registry.director.run";
-export const BASE_PATH = appConfig?.basePath || "/";
+export const BASE_PATH: string = appConfig?.basePath || "/";
 
 console.log("config is", { GATEWAY_URL, REGISTRY_URL, BASE_PATH });

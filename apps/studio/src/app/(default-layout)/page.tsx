b74c88c9bd27afb281358f03cbad68174714c040
Barnaby Malet <bmalet@gmail.com>
2025-09-24 21:39:34 +0200

    fix

--- apps/studio/src/app/(default-layout)/page.tsx
+++ apps/studio/src/app/(default-layout)/page.tsx
@@ -1,35 +1,8 @@
 "use client";
 
-import { Inter, JetBrains_Mono } from "next/font/google";
 import { FullScreenError } from "../../components/pages/global/error";
 
-import "../globals.css";
-
-const sans = Inter({
-  variable: "--font-sans",
-  subsets: ["latin"],
-});
-
-const mono = JetBrains_Mono({
-  variable: "--font-mono",
-  subsets: ["latin"],
-});
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`${sans.variable} ${mono.variable} antialiased`}>
-        <PleaseUpdate />
-      </body>
-    </html>
-  );
-}
-
-function PleaseUpdate() {
+export default function PleaseUpdate() {
   return (
     <FullScreenError
       title={"Please Update Director"}

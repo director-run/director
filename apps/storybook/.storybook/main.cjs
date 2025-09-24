3e81cb8ef253e715231de9356e71cd96866664a7
Barnaby Malet <bmalet@gmail.com>
2025-09-24 22:20:54 +0200

    ok

--- apps/storybook/.storybook/main.cjs
+++ apps/storybook/.storybook/main.cjs
@@ -24,7 +24,10 @@ const config = {
       },
       resolve: {
         alias: {
-          "@director.run/design": resolve(__dirname, "../../studio/src"),
+          "@director.run/design": resolve(
+            __dirname,
+            "../../../packages/design/src",
+          ),
         },
       },
     });

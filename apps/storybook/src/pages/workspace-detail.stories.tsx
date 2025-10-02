cdf68c439f63e7869185934e21aea395cecbcd2c
Barnaby Malet <bmalet@gmail.com>
2025-10-02 16:12:31 +0200

    workspace detail stories

--- apps/storybook/src/pages/workspace-detail.stories.tsx
+++ apps/storybook/src/pages/workspace-detail.stories.tsx
@@ -1,9 +1,16 @@
+import {
+  SplitView,
+  SplitViewMain,
+  SplitViewSide,
+} from "@director.run/design/components/split-view.tsx";
 import type {
   Client,
   MCPTool,
   WorkspaceDetail,
 } from "@director.run/design/components/types.ts";
+import { Container } from "@director.run/design/components/ui/container.tsx";
 import { WorkspaceDetailContent } from "@director.run/design/components/workspaces/workspace-detail-content.tsx";
+import { WorkspaceSectionClients } from "@director.run/design/components/workspaces/workspace-section-clients.tsx";
 import { mockTools } from "@director.run/design/test/fixtures/mcp/tools.js";
 import { mockClients } from "@director.run/design/test/fixtures/workspace/clients.ts";
 import { mockWorkspace } from "@director.run/design/test/fixtures/workspace/workspace.ts";
@@ -14,18 +21,34 @@ import { withLayoutView } from "../helpers/decorators";
 const WorkspaceDetailComponent = ({
   workspace,
   tools,
+  clients,
 }: {
   workspace: WorkspaceDetail;
   clients: Client[];
   tools: McpSdkTool[];
 }) => (
-  <WorkspaceDetailContent
-    workspace={workspace}
-    tools={tools as MCPTool[]}
-    toolsLoading={false}
-    onClickServer={() => console.log("library click")}
-    onClickAddServer={() => console.log("server click")}
-  />
+  <Container size="xl">
+    <SplitView>
+      <SplitViewMain>
+        <WorkspaceDetailContent
+          workspace={workspace}
+          tools={tools as MCPTool[]}
+          toolsLoading={false}
+          onClickServer={() => console.log("library click")}
+          onClickAddServer={() => console.log("server click")}
+        />
+      </SplitViewMain>
+      <SplitViewSide>
+        <WorkspaceSectionClients
+          workspace={workspace}
+          gatewayBaseUrl={"https://some.url.com"}
+          clients={clients ?? []}
+          onChangeInstall={() => console.log("change install")}
+          isLoading={false}
+        />
+      </SplitViewSide>
+    </SplitView>
+  </Container>
 );
 
 const meta = {

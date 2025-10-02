88df45a8ac61c122084d9cb6d9f8458646e88fcb
Barnaby Malet <bmalet@gmail.com>
2025-10-02 15:55:33 +0200

    tidy client connections

--- packages/design/src/components/workspaces/workspace-section-clients.tsx
+++ packages/design/src/components/workspaces/workspace-section-clients.tsx
@@ -67,7 +67,7 @@ function ProxyInstallers({
   onChangeInstall,
 }: ProxyInstallersProps) {
   return (
-    <div className="grid grid-cols-1 gap-1 sm:grid-cols-2 lg:grid-cols-3">
+    <div className="grid grid-cols-1 gap-1">
       {clients.map((it) => (
         <label
           htmlFor={it.id}

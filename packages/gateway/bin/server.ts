bec9e6d2768347dcc26993d145043c2a1a2e2a12
Barnaby Malet <bmalet@gmail.com>
2025-11-22 23:25:31 +0100

    ok

--- packages/gateway/bin/server.ts
+++ packages/gateway/bin/server.ts
@@ -1,7 +1,12 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import { findFirstMatch } from "@director.run/utilities/fs";
+import { getLogger } from "@director.run/utilities/logger";
 import { createStore } from "../src/db";
 import { DATABASE_URL, SERVER_PORT } from "../src/env";
 import { Gateway } from "../src/gateway";
 
+const logger = getLogger("Server");
 async function start() {
   const dbStore = createStore({ connectionString: DATABASE_URL }).playbooks;
 
@@ -9,6 +14,7 @@ async function start() {
     dbStore,
     baseUrl: `http://localhost:${SERVER_PORT}`,
     port: SERVER_PORT,
+    studioAssetsPath: getStudioAssetsPath(),
     oauth: {
       storage: "memory",
       baseCallbackUrl: `http://localhost:${SERVER_PORT}`,
@@ -17,3 +23,20 @@ async function start() {
 }
 
 await start();
+
+const getStudioAssetsPath = (): string | undefined => {
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+  const candidates = [
+    path.join(__dirname, "../../../dist/studio/index.html"), // development
+    path.join(__dirname, "./studio/index.html"), // compiled JS
+  ];
+
+  logger.debug({
+    message: "attempting to resolve studio dist path",
+    candidates,
+  });
+
+  const match = findFirstMatch(candidates);
+  return match ? path.dirname(match) : undefined;
+};

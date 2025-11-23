40930da44208702a5a4bfe6cbc0eb9b8d2114642
Barnaby Malet <bmalet@gmail.com>
2025-11-23 11:21:31 +0100

    ok

--- packages/gateway/bin/server.ts
+++ packages/gateway/bin/server.ts
@@ -1,38 +1,44 @@
 import path from "path";
 import { fileURLToPath } from "url";
-import { findFirstMatch } from "@director.run/utilities/fs";
-import { getLogger } from "@director.run/utilities/logger";
 import { createStore } from "../src/db";
 import { DATABASE_URL, SERVER_PORT } from "../src/env";
 import { Gateway } from "../src/gateway";
 
-const logger = getLogger("Server");
+// const logger = getLogger("Server");
 
-const getStudioAssetsPath = (): string | undefined => {
-  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+// const getStudioAssetsPath = (): string | undefined => {
+//   const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-  const candidates = [
-    path.join(__dirname, "../../../dist/studio/index.html"), // development
-    path.join(__dirname, "./studio/index.html"), // compiled JS
-  ];
+//   const candidates = [
+//     path.join(__dirname, "../../../dist/studio/index.html"), // development
+//     path.join(__dirname, "./studio/index.html"), // compiled JS
+//   ];
 
-  logger.debug({
-    message: "attempting to resolve studio dist path",
-    candidates,
-  });
+//   logger.debug({
+//     message: "attempting to resolve studio dist path",
+//     candidates,
+//   });
 
-  const match = findFirstMatch(candidates);
-  return match ? path.dirname(match) : undefined;
-};
+//   const match = findFirstMatch(candidates);
+//   return match ? path.dirname(match) : undefined;
+// };
 
 async function start() {
   const dbStore = createStore({ connectionString: DATABASE_URL }).playbooks;
+
+  console.log("--------------------------------");
+  console.log("--------------------------------");
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const assetsPath = path.join(__dirname, "./studio/index.html");
+  console.log(assetsPath);
+  console.log("--------------------------------");
+  console.log("--------------------------------");
 
   await Gateway.start({
     dbStore,
     baseUrl: `http://localhost:${SERVER_PORT}`,
     port: SERVER_PORT,
-    studioAssetsPath: getStudioAssetsPath(),
+    studioAssetsPath: assetsPath,
     oauth: {
       storage: "memory",
       baseCallbackUrl: `http://localhost:${SERVER_PORT}`,
